0ecee33ec3ff161890c0dbf88683eaf4619bd1f6
[dyad] <fariamisael8@gmail.com>
2026-06-12 21:25:02 -0300

    [dyad] Rota padrão configurada com redirect - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import { Toaster } from "@/components/ui/sonner";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
@@ -10,6 +10,7 @@ const App = () => (
       <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
     <Toaster />
   </BrowserRouter>
